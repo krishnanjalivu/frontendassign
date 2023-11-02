6b0ac2402dcf45ea7bd08a2489a829d2dc8b9a7f
krishnanjalivu <vukrishnanjali@gmail.com>
2023-11-02 21:38:38 +0530

    first commit

---
+++ client/src/scenes/dashboard/index.tsx
@@ -0,0 +1,86 @@
+import { Box, useMediaQuery } from "@mui/material";
+
+const gridTemplateLargeScreens = `
+"a b c"
+"a b c"
+"a b c"
+"a b f"
+"d e f"
+"d e f"
+"d h i"
+"g h i"
+"g h j"
+"g h j"
+
+`;
+const gridTemplateSmallScreens = `
+"a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+
+`;
+
+const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
+
+  return (
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
+    >
+      <Box bgcolor="white" gridArea="a"></Box>
+      <Box bgcolor="white" gridArea="b"></Box>
+      <Box bgcolor="white" gridArea="c"></Box>
+      <Box bgcolor="white" gridArea="d"></Box>
+      <Box bgcolor="white" gridArea="e"></Box>
+      <Box bgcolor="white" gridArea="f"></Box>
+      <Box bgcolor="white" gridArea="g"></Box>
+      <Box bgcolor="white" gridArea="h"></Box>
+      <Box bgcolor="white" gridArea="i"></Box>
+      <Box bgcolor="white" gridArea="j"></Box>
+    </Box>
+  );
+};
+export default Dashboard;
